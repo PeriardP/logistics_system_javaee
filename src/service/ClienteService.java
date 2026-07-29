b6d30ff6554b80c4ba3cb3ef6fd9f6601b14adfd
Pedro Periard <periardpedro06@gmail.com>
2026-07-29 20:11:28 -0300

    cliente service com as restriçoes

--- src/service/ClienteService.java
+++ src/service/ClienteService.java
@@ -16,28 +16,37 @@ public class ClienteService extends GenericService<Cliente>{
 		super(Cliente.class);
 	}
 	
-public List<Cliente> pesquisarPorNome(String texto){
 	
-	final CriteriaBuilder cBuilder =  getEntityManager().getCriteriaBuilder();
-	final CriteriaQuery<Cliente> cQuery = cBuilder.createQuery(Cliente.class);
-	final Root<Cliente> rootCliente = cQuery.from(Cliente.class);
-	
-	cQuery.select(rootCliente);
-	
-	final Expression<String> expNome = rootCliente.get("nome");
-	
-	cQuery.where(cBuilder.like(expNome, "%"+texto+"%"));
-	cQuery.orderBy(cBuilder.asc(expNome));
-	
-	List<Cliente> resultado = getEntityManager().createQuery(cQuery).getResultList();
-	return resultado;
-  }
+	public List<Cliente> pesquisarPorNome(String texto){
+		
+		final CriteriaBuilder cBuilder =  getEntityManager().getCriteriaBuilder();
+		final CriteriaQuery<Cliente> cQuery = cBuilder.createQuery(Cliente.class);
+		final Root<Cliente> rootCliente = cQuery.from(Cliente.class);
+		
+		cQuery.select(rootCliente);
+		
+		final Expression<String> expNome = rootCliente.get("nome");
+		
+		cQuery.where(cBuilder.like(expNome, "%"+texto+"%"));
+		cQuery.orderBy(cBuilder.asc(expNome));
+		
+		List<Cliente> resultado = getEntityManager().createQuery(cQuery).getResultList();
+		return resultado;
+	}
+
+	// não permitir CPF duplicado
+	public boolean existeCpf(String cpf) {
+		CriteriaBuilder cBuilder = getEntityManager().getCriteriaBuilder();
+		CriteriaQuery<Long> cQuery = cBuilder.createQuery(Long.class);
+		Root<Cliente> rootCliente = cQuery.from(Cliente.class);
+		
+		// procura no bd quantos clientes têm esse CPF
+		cQuery.select(cBuilder.count(rootCliente));
+		cQuery.where(cBuilder.equal(rootCliente.get("cpf"), cpf));
+		
+		Long quantidade = getEntityManager().createQuery(cQuery).getSingleResult();
+		
+		
+		return quantidade > 0;
+	}
 }
-	
-	
-	
-	
-	
-
-
-
