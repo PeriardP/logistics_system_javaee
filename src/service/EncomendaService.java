42bd36a72a4bed88a1ba0b6543a291d305043c83
Pedro Periard <periardpedro06@gmail.com>
2026-07-30 21:06:51 -0300

    arrumei o encomenda service

--- src/service/EncomendaService.java
+++ src/service/EncomendaService.java
@@ -1,11 +1,13 @@
 package service;
 
+import java.util.ArrayList;
 import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 
 import logistica.modelo.Encomenda;
@@ -20,13 +22,42 @@ public class EncomendaService extends GenericService<Encomenda> {
         super(Encomenda.class);
     }
 
-    // Listar as encomendas usando o Criteria 
+    // Metodo usado pela tela de Registro de Encomendas 
     public List<Encomenda> pesquisarTodas() {
         CriteriaBuilder cb = em.getCriteriaBuilder();
         CriteriaQuery<Encomenda> cq = cb.createQuery(Encomenda.class);
         Root<Encomenda> root = cq.from(Encomenda.class);
         
         cq.select(root);
+        
+        return em.createQuery(cq).getResultList();
+    }
+
+    // Metodo usado pela tela de Relatórios
+    public List<Encomenda> relatorioEntregas(Long idEntregador, String cidade, Double valorMaximo) {
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<Encomenda> cq = cb.createQuery(Encomenda.class);
+        Root<Encomenda> root = cq.from(Encomenda.class);
+        
+        List<Predicate> predicados = new ArrayList<Predicate>();
+        
+        if (idEntregador != null) {
+            predicados.add(cb.equal(root.get("entregador").get("id"), idEntregador));
+        }
+        
+        if (cidade != null && !cidade.trim().isEmpty()) {
+            predicados.add(cb.like(root.get("cliente").get("endereco").get("cidade"), "%" + cidade + "%"));
+        }
+        
+        if (valorMaximo != null) {
+            predicados.add(cb.lessThan(root.get("valor"), valorMaximo));
+        }
+        
+        if (!predicados.isEmpty()) {
+            cq.where(cb.and(predicados.toArray(new Predicate[0])));
+        }
+        
+        cq.orderBy(cb.asc(root.get("cliente").get("nome")));
         
         return em.createQuery(cq).getResultList();
     }
